8b702c67246cc7d97864aa01e4efd72bb85720bf
Addie Padhi <aaditipadhi@umass.edu>
2023-10-28 14:35:55 -0400

    updating formatting

--- assets/js/topbar.js
+++ assets/js/topbar.js
@@ -1,6 +1,6 @@
 $(function () {
     $(document).scroll(function () {
         var $nav = $(".topbar-fixed");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $nav.toggleClass('scrolled', $(this).scrollTop() > 700);
       });
   });
