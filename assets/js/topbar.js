054e4bbc3e63e2c377ce01e0a79f1d69329a678e
Addie Padhi <aaditipadhi@umass.edu>
2023-10-28 14:49:21 -0400

    experimenting with dynamic topbar

--- assets/js/topbar.js
+++ assets/js/topbar.js
@@ -4,3 +4,17 @@ $(function () {
         $nav.toggleClass('scrolled', $(this).scrollTop() > 700);
       });
   });
+
+var initalPos = window.scrollY;
+window.onscroll = function() {
+var currPos = window.scrollY;
+if (currPos < 100) {
+    document.getElementById("topbar").style.top = "0";
+}
+else if (initalPos > currPos) {
+    document.getElementById("topbar").style.top = "0";
+} else {
+    document.getElementById("topbar").style.top = "-50px";
+}
+initalPos = currPos;
+}
