f5008018c4e06c397ab78038659e5aa59871485c
Addie Padhi <aaditipadhi@umass.edu>
2023-10-30 13:00:19 -0400

    fixed conditional formatting for navbar

--- assets/js/topbar.js
+++ assets/js/topbar.js
@@ -1,20 +1,33 @@
+// gets positions of elements (top)
+function getOffset(el) {
+    const rect = el.getBoundingClientRect();
+    return {
+        top: rect.top + window.scrollY
+    };
+}
+
+// position of main content (below video loop)
+var mainDiv = document.getElementById('mainContent')
+console.log(getOffset(mainDiv).top)
+
+// add conditional formatting when scrolled past main content
 $(function () {
     $(document).scroll(function () {
         var $nav = $(".topbar-fixed");
-        $nav.toggleClass('scrolled', $(this).scrollTop() > 700);
+        $nav.toggleClass('scrolled', $(this).scrollTop() > getOffset(mainDiv).top);
       });
   });
 
 var initalPos = window.scrollY;
 window.onscroll = function() {
 var currPos = window.scrollY;
-if (currPos < 100) {
+if (currPos < 750) {
     document.getElementById("topbar").style.top = "0";
 }
 else if (initalPos > currPos) {
     document.getElementById("topbar").style.top = "0";
 } else {
-    document.getElementById("topbar").style.top = "-50px";
+    document.getElementById("topbar").style.top = "-100px";
 }
 initalPos = currPos;
 }
